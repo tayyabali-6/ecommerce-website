903e69e4f9ac2c2ff18244d5ed4de88ddb4c0975
Tayyab Ali <ta7396272@gmail.com>
2025-06-29 15:15:25 +0100

    Fix: correct ForgotPassword import casing

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Register from './Register'
 import Login from './Login'
-import ForgotPassword from './forgotPassword'
+import ForgotPassword from './ForgotPassword'
 
 const Auth = () => {
     return (
@@ -9,7 +9,7 @@ const Auth = () => {
             <Routes>
                 <Route path='register' element={<Register />} />
                 <Route path='login' element={<Login />} />
-                <Route path='forgot-password' element={<ForgotPassword/>} />
+                <Route path='forgot-password' element={<ForgotPassword />} />
             </Routes>
 
         </div>
